cbd5819fc46bb04ff5854e59860d22e8ed5e18a9
Shun Yao <ysmiracle@hotmail.com>
2018-04-09 02:09:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,24 @@
 class Artist
-  attr_accessor :name, :songs, :genres
-  
+  #attr_accessor :name, :songs, :genres
+
   def initialize(name)
     @name = name
     @songs = []
-    @genres = []
   end
 
   def add_song(song)
     self.songs << song
     song.artist = self
     genres << song.genre
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
   end
 end

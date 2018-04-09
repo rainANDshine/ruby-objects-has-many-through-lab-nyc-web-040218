8f38ed6c5363165d77428c112c514a8ffd3df4de
Shun Yao <ysmiracle@hotmail.com>
2018-04-09 02:14:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 class Song
   #attr_accessor :name, :songs, :genres
-  attr_accessor :artist
+  attr_accessor :artist, :genre
 
   def initialize(name, genre)
     @name = name
     @genre = genre
   end
-  
+
   def genre
     @genre
   end
